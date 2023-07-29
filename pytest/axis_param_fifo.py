8228f72bb4729efcf046c6dfbc669fc8d12857a1
Andrey <andvasilc3@mail.ru>
2023-07-29 17:11:01 +0600

    removed unused modules from files;
    removed unnecessary cocotb.startsoon statements;
    using vars instead of dir in main.py;
    refactored function transaction_drivers in axis_param_fifo.py to support *args;
    added pkts_test to axis_param_fifo.py;
    refactored AXISDriver base class init: now it checks for signals with postfix from AXIS.Payload field names, and after that checks if parameters for width of these fields exist;
    refactored gen_rnd_pld in AXISDriver: now it receives AXIS.Payload with widths for each field instead of max for separate signals;
    added gen_rnd_pld in AXISDriverM: it generates payload based on dut axis params;
    added rcv_pkt in AXISDriverS: function gets one axis packet from dut.

--- pytest/axis_param_fifo.py
+++ pytest/axis_param_fifo.py
@@ -1,27 +1,24 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import Timer, ClockCycles, FallingEdge, RisingEdge
-from cocotb.types import LogicArray
-from cocotb.binary import BinaryValue
-import numpy as np
-from axis import AXIS
+from cocotb.triggers import ClockCycles
+# noinspection PyUnresolvedReferences
 from axis_drivers import AXISDriverM, AXISDriverS
-from typing import List
 
 
-async def transaction_driver(driver_func, perc, repeat_times):
+async def transaction_driver(repeat_times, driver_func, *args):
     arr = []
     for _ in range(repeat_times):
-        arr.append(await cocotb.start_soon(driver_func(perc)))
+        arr.append(await driver_func(*args))
     return arr
 
 
 async def init_driver(dut):
     axis_driver_m = AXISDriverM()
     axis_driver_s = AXISDriverS()
+    # noinspection PyAsyncCall
     cocotb.start_soon(Clock(dut.clk, 2, units="ns").start())
-    await cocotb.start_soon(axis_driver_m.clear_states())
-    await cocotb.start_soon(axis_driver_s.clear_states())
+    await axis_driver_m.clear_states()
+    await axis_driver_s.clear_states()
     dut.rst.value = 1
     await ClockCycles(dut.clk, 100)
     dut.rst.value = 0
@@ -34,8 +31,8 @@ async def constant_rw_test(dut):
 
     nw = int(dut.NUMWORDS)
 
-    written_pld = cocotb.start_soon(transaction_driver(axis_driver_m.send_rnd_pld, 100, nw))
-    read_pld = await cocotb.start_soon(transaction_driver(axis_driver_s.rcv_pld, 100, nw))
+    written_pld = cocotb.start_soon(transaction_driver(nw, axis_driver_m.send_rnd_pld, 100))
+    read_pld = await transaction_driver(nw, axis_driver_s.rcv_pld, 100)
 
     passed = written_pld.result() == read_pld
     assert passed
@@ -46,8 +43,8 @@ async def random_rw_test(dut):
 
     nw = int(dut.NUMWORDS)
 
-    written_pld = cocotb.start_soon(transaction_driver(axis_driver_m.send_rnd_pld, 50, nw))
-    read_pld = await cocotb.start_soon(transaction_driver(axis_driver_s.rcv_pld, 50, nw))
+    written_pld = cocotb.start_soon(transaction_driver(nw, axis_driver_m.send_rnd_pld, 50))
+    read_pld = await transaction_driver(nw, axis_driver_s.rcv_pld, 50)
 
     passed = written_pld.result() == read_pld
     assert passed
@@ -58,14 +55,14 @@ async def wr_rd_test(dut):
 
     nw = int(dut.NUMWORDS)
 
-    written_pld = cocotb.start_soon(transaction_driver(axis_driver_m.send_rnd_pld, 100, nw * 2))
+    written_pld = cocotb.start_soon(transaction_driver(nw*2, axis_driver_m.send_rnd_pld, 100))
 
     while True:
         await ClockCycles(dut.clk, 1)
         if dut.i_axis_param_fifo.full.value:
             break
 
-    read_pld = await cocotb.start_soon(transaction_driver(axis_driver_s.rcv_pld, 100, nw * 2))
+    read_pld = await transaction_driver(nw*2, axis_driver_s.rcv_pld, 100)
 
     passed = written_pld.result() == read_pld
     assert passed
@@ -76,13 +73,23 @@ async def rd_wr_test(dut):
 
     nw = int(dut.NUMWORDS)
 
-    read_pld = cocotb.start_soon(transaction_driver(axis_driver_s.rcv_pld, 100, nw * 2))
+    read_pld = cocotb.start_soon(transaction_driver(nw*2, axis_driver_s.rcv_pld, 100))
 
     await ClockCycles(dut.clk, nw)
 
-    written_pld = cocotb.start_soon(transaction_driver(axis_driver_m.send_rnd_pld, 100, nw * 2))
+    written_pld = cocotb.start_soon(transaction_driver(nw*2, axis_driver_m.send_rnd_pld, 100))
 
     await ClockCycles(dut.clk, nw*3)
 
     passed = written_pld.result() == read_pld.result()
+    assert passed
+
+
+async def pkts_test(dut):
+    axis_driver_m, axis_driver_s, dut = await init_driver(dut)
+
+    written_pld = cocotb.start_soon(transaction_driver(100, axis_driver_m.send_rnd_pkt, 10, 20, 100))
+    read_pld = await transaction_driver(100, axis_driver_s.rcv_pkt, 100, False)
+
+    passed = written_pld.result() == read_pld
     assert passed
--- pytest/tools/axis_drivers.py
+++ pytest/tools/axis_drivers.py
@@ -1,6 +1,5 @@
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import Timer, ClockCycles, FallingEdge, RisingEdge
+from cocotb.triggers import ClockCycles
 from cocotb.types import LogicArray
 from cocotb.binary import BinaryValue
 import numpy as np
@@ -16,17 +15,24 @@ class AXISDriver:
         self.__driver_type = driver_type
         self.__dut = cocotb.top
         if driver_type == "Master":
-            width_prefix = "WIDTH_AXIS_M_"
             self.__signal_prefix = "axis_m_"
         elif driver_type == "Slave":
-            width_prefix = "WIDTH_AXIS_S_"
             self.__signal_prefix = "axis_s_"
-        variable_width_fields = ["TDATA", "TUSER", "TID", "TKEEP"]
+
         self._params_dict = {}
-        for variable_width_field in variable_width_fields:
-            current_width = int(getattr(self.__dut, width_prefix + variable_width_field))
-            self._params_dict[variable_width_field] = {"WIDTH": current_width,
-                                                       "MAX": 2 ** current_width - 1}
+
+        for payload_field in vars(AXIS.Payload()):
+            signal_name = self.__signal_prefix+payload_field
+            if hasattr(self.__dut, signal_name):
+                param_name = "WIDTH_" + signal_name.upper()
+                if hasattr(self.__dut, param_name):
+                    current_width = int(getattr(self.__dut, param_name))
+                else:
+                    current_width = getattr(self.__dut, signal_name).value.n_bits
+                self._params_dict[payload_field.upper()] = {"WIDTH": current_width}
+            else:
+                pass
+
         self.__axis_payload = AXIS.Payload(0, 0, 0, 0, 0)
         self._clk = getattr(self.__dut, self.__signal_prefix + "clk")
         self._rst = getattr(self.__dut, self.__signal_prefix + "rst")
@@ -36,11 +42,11 @@ class AXISDriver:
     @property
     def _axis_payload(self):
         axis_payload = AXIS.Payload()
-        for field_type in self._params_dict:
-            ft_lower = field_type.lower()
-            if self._params_dict[field_type]["WIDTH"] > 0:
-                cur_state = getattr(self.__dut, self.__signal_prefix + ft_lower).value
-                setattr(axis_payload, ft_lower, cur_state)
+        for payload_field in self._params_dict:
+            pf_lower = payload_field.lower()
+            if self._params_dict[payload_field]["WIDTH"] > 0:
+                cur_state = getattr(self.__dut, self.__signal_prefix + pf_lower).value
+                setattr(axis_payload, pf_lower, cur_state)
         self.__axis_payload = axis_payload
         return self.__axis_payload
 
@@ -49,16 +55,12 @@ class AXISDriver:
         if self.__driver_type == "Master":
             self.__axis_payload = new_axis_payload
 
-            self.__dut.axis_m_tlast.value = LogicArray(
-                'X') if self.__axis_payload.tlast is None \
-                else self.__axis_payload.tlast
-
-            for field_type in self._params_dict:
-                if self._params_dict[field_type]["WIDTH"] > 0:
-                    cur_field_py_value = getattr(self.__axis_payload, field_type.lower())
-                    cur_field_hdl = getattr(self.__dut, "axis_m_" + field_type.lower())
+            for payload_field in self._params_dict:
+                if self._params_dict[payload_field]["WIDTH"] > 0:
+                    cur_field_py_value = getattr(self.__axis_payload, payload_field.lower())
+                    cur_field_hdl = getattr(self.__dut, "axis_m_" + payload_field.lower())
                     cur_field_hdl.value = LogicArray(
-                        'X' * self._params_dict[field_type]["WIDTH"]) if cur_field_py_value is None \
+                        'X' * self._params_dict[payload_field]["WIDTH"]) if cur_field_py_value is None \
                         else cur_field_py_value
         elif self.__driver_type == "Slave":
             raise ValueError("Slave payload can't be set")
@@ -81,30 +83,31 @@ class AXISDriver:
             self._axis_payload = AXIS.Payload(0, 0, 0, 0, 0)
 
     @staticmethod
-    def gen_rnd_pld(max_tdata, max_tuser, max_tid, max_tkeep, state_tlast="rnd"):
-        allowed_states_tlast = ["rnd", "1", "0"]
-        if state_tlast not in allowed_states_tlast:
-            raise ValueError(
-                f"gen_rnd_pld - invalid value in state_tlast: {state_tlast}; \n"
-                f"Allowed values: {allowed_states_tlast}")
-        if state_tlast == "rnd":
-            tlast_value = np.random.randint(0, 2)
-        elif state_tlast == "1":
-            tlast_value = 1
-        elif state_tlast == "0":
-            tlast_value = 0
-        else:
-            tlast_value = None
-        return AXIS.Payload(tlast=tlast_value,
-                            tdata=np.random.randint(0, max_tdata) if max_tdata > 0 else 0,
-                            tuser=np.random.randint(0, max_tuser) if max_tuser > 0 else 0,
-                            tid=np.random.randint(0, max_tid) if max_tid > 0 else 0,
-                            tkeep=np.random.randint(0, max_tkeep) if max_tkeep > 0 else 0)
+    def gen_rnd_pld(axis_payload_widths: AXIS.Payload):
+
+        axis_payload = AXIS.Payload()
+
+        for payload_field in vars(AXIS.Payload()):
+            field_width = getattr(axis_payload_widths, payload_field)
+            if field_width is not None:
+                max_value = 2**field_width
+                field_value = np.random.randint(0, max_value) if max_value > 1 else None
+            else:
+                field_value = None
+            setattr(axis_payload, payload_field, field_value)
+
+        return axis_payload
 
 
 class AXISDriverM(AXISDriver):
     def __init__(self):
         super().__init__("Master")
+
+    def gen_rnd_pld(self):
+        axis_payload_widths = AXIS.Payload()
+        for payload_field in self._params_dict:
+            setattr(axis_payload_widths, payload_field.lower(), self._params_dict[payload_field]["WIDTH"])
+        return AXISDriver.gen_rnd_pld(axis_payload_widths)
 
     async def send_pld(self, axis_payload: AXIS.Payload, send_chance=100):
         if send_chance > 100:
@@ -120,35 +123,35 @@ class AXISDriverM(AXISDriver):
                 tvalid_set = True
             await ClockCycles(self._clk, 1)
             if self._handshake:
-                await cocotb.start_soon(self.clear_states())
+                await self.clear_states()
                 return self._axis_payload
 
     async def send_rnd_pld(self, send_chance=100):
-        return await cocotb.start_soon(self.send_pld(
-            AXISDriver.gen_rnd_pld(self._params_dict["TDATA"]["MAX"], self._params_dict["TUSER"]["MAX"],
-                                   self._params_dict["TID"]["MAX"], self._params_dict["TKEEP"]["MAX"]), send_chance))
+        return await self.send_pld(self.gen_rnd_pld(), send_chance)
 
     async def send_rnd_pkt(self, min_size: int, max_size: int, chance=100):
-        if min_size < max_size:
-            pkt_size = np.random.randint(min_size, max_size)
-        elif min_size == max_size:
-            pkt_size = min_size
+        pkt_size = None
+        if min_size <= max_size:
+            pkt_size = np.random.randint(min_size, max_size+1)
         elif min_size > max_size:
             raise ValueError(f"send_rnd_pkt - min_size={min_size} is less than max_size={max_size}")
         chances = [chance] * pkt_size
-        axis_payloads = [AXISDriver.gen_rnd_pld(self._params_dict["TDATA"]["MAX"],
-                                                self._params_dict["TUSER"]["MAX"],
-                                                self._params_dict["TID"]["MAX"],
-                                                self._params_dict["TKEEP"]["MAX"],
-                                                "0" if _ < (pkt_size - 1) else "1") for _ in range(pkt_size)]
-        return await cocotb.start_soon(self.send_plds(axis_payloads, chances))
+        axis_payloads = []
+        for sample_num in range(pkt_size):
+            axis_payload = self.gen_rnd_pld()
+            if sample_num < (pkt_size - 1):
+                axis_payload.tlast = 0
+            else:
+                axis_payload.tlast = 1
+            axis_payloads.append(axis_payload)
+        return await self.send_plds(axis_payloads, chances)
 
     async def send_plds(self, axis_payloads: List[AXIS.Payload], chances=None):
         if chances is None:
             chances = [100] * len(axis_payloads)
         sent_payloads = []
         for axis_payload, chance in zip(axis_payloads, chances):
-            sent_payloads.append(await cocotb.start_soon(self.send_pld(axis_payload, chance)))
+            sent_payloads.append(await self.send_pld(axis_payload, chance))
         return sent_payloads
 
 
@@ -166,8 +169,17 @@ class AXISDriverS(AXISDriver):
             if cur_chance <= rcv_chance:
                 self._handshake = 1
             elif not tready_stays:
-                await cocotb.start_soon(self.clear_states())
+                await self.clear_states()
             await ClockCycles(self._clk, 1)
             if self._handshake:
-                await cocotb.start_soon(self.clear_states())
+                await self.clear_states()
                 return self._axis_payload
+
+    async def rcv_pkt(self, rcv_chance=100, tready_stays=False):
+        axis_payloads = []
+        while True:
+            axis_payload = await self.rcv_pld(rcv_chance, tready_stays)
+            axis_payloads.append(axis_payload)
+            if axis_payload.tlast == 1:
+                break
+        return axis_payloads
